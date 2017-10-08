25e3170d2a7f7fbffd6208ac8288b9c07fda2ff0
inscode <dongdong11019@gmail.com>
2017-10-08 23:40:42 +0800

    手风琴优化

---
+++ 8/js/code_5.js
@@ -0,0 +1,9 @@
+function showDl(object) {
+	var dlId = object.getElementsByTagName('dl')[0];
+	dlId.style.display = 'block';
+}
+
+function hideDl(object) {
+	var dlId = object.getElementsByTagName('dl')[0];
+	dlId.style.display = 'none';
+}
