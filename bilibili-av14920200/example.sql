52d56eb1ae7268380dbbe52b7e323c777f6b1a84
inscode <dongdong11019@gmail.com>
2017-11-04 22:00:40 +0800

    索引

--- bilibili-av14920200/example.sql
+++ bilibili-av14920200/example.sql
@@ -4,3 +4,12 @@ create table example2(
     sex boolean,
     primary key(id,name)
 );
+
+create database if not exist swartz default charset utf8mb4;
+
+create table index2(
+    id int UNIQUE,
+    name varchar(20),
+    sex boolean,
+    UNIQUE index index2_id(id asc)
+);
