fa96c616d02e44037394d867d13cf899363d6bbd
inscode <dongdong11019@gmail.com>
2017-11-05 22:33:37 +0800

    rename

---
+++ imooc-8/js/code_4.js
@@ -0,0 +1,9 @@
+function showDl(id) {
+	var dlId = document.getElementById(id);
+	dlId.style.display = 'block';
+}
+
+function hideDl(id) {
+	var dlId = document.getElementById(id);
+	dlId.style.display = 'none';
+}
