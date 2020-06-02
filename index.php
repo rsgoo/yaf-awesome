b7ecdbed95d31d797fd2912d97b0345808972c7a
inscode <dongdong11019@gmail.com>
2020-06-02 23:27:49 +0800

    ini

--- index.php
+++ index.php
@@ -3,7 +3,21 @@
 /* 定义这个常量是为了在application.ini中引用*/
 define('APPLICATION_PATH', dirname(__FILE__));
 
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
+// 自定义数组配置
+/*
+$config = array(
+    "application" => array(
+        "directory" => realpath(dirname(__FILE__)) . "/application",
+    ),
+);
+$application = new Yaf_Application($config);
+*/
+
+// 默认 ini 文件配置模式
+$application = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
+
+//$application->execute("main", $argc, $argv);
+//通常用于在cron任务中运行Yaf_Application。 在cron任务中也可以使用autoloader和Bootstrap机制。
 
